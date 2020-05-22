1e9942c0748a18df2dd1a87536481e1550a23f8d
Elshan <mytwink21@mail.ru>
2020-05-22 13:37:13 +0400

    Changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -14,3 +14,21 @@ for (let item of formInputs) {
     }
   });
 }
+
+// Right-menu
+
+const MobileMenuToggle = document.querySelector(".toggle-menu");
+const mobMenu = document.querySelector(".header-wrapper__nav");
+const overlay = document.querySelector("#overlay");
+
+MobileMenuToggle.addEventListener("click", function () {
+  mobMenu.classList.toggle("active");
+  this.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+window.addEventListener("resize", function () {
+  mobMenu.classList.remove("active");
+  MobileMenuToggle.classList.remove("active");
+  overlay.classList.remove("active");
+});
